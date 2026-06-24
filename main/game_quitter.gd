7d8d7ed4ffb6b7ef14651c9cc3287c085d23b0b5
Demetrius Dixon <wilodemetrius@gmail.com>
2026-06-24 10:45:29 -0400

    Quitting the game now unregisters the client from the ingest

--- main/game_quitter.gd
+++ main/game_quitter.gd
@@ -2,5 +2,7 @@ extends Node
 
 func quit_monkanics() -> void:
 	
+	Client.unregister_from_ingest_server()
+	
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 	get_tree().quit()
