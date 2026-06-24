37e36d3bfe441b1a3d58655c0b37eff7fbfcb31f
Demetrius Dixon <wilodemetrius@gmail.com>
2026-06-24 10:41:43 -0400

    Gameplay_Manager script setup

--- multiplayer/gameplay/gameplay_manager.gd
+++ multiplayer/gameplay/gameplay_manager.gd
@@ -1,1 +1,20 @@
 extends Node
+
+var Main_Menu_Scene : PackedScene = preload("uid://cuetqgx13tv6s")
+var Gameplay_Scene : PackedScene = preload("uid://f11ymr6ma2f7")
+
+func _ready() -> void:
+	
+	if OS.has_feature("dedicated_server") \
+	or OS.has_feature("ingest") \
+	or OS.has_feature("relay"):
+		
+		queue_free()
+		
+	else:
+		
+		load_main_menu()
+
+func load_main_menu() -> void:
+	
+	pass
