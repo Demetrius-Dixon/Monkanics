545f66378f32120864ebb9a8ab6bdc68feb270c1
Demetrius Dixon <wilodemetrius@gmail.com>
2026-06-25 06:23:01 -0400

    "Play Online" button now registers the client to the ingest

--- ui and hud/main menu/main_menu.gd
+++ ui and hud/main menu/main_menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
 
+func _on_play_online_pressed() -> void:
+	
+	Client.register_to_ingest_server()
+	
+	
+	
+	#TODO Add sub-menu later in development
+
 func _on_quit_game_pressed() -> void:
 	GameQuitter.quit_monkanics()
