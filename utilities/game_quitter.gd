e784c7227b26ffed515f0fff6d3b682cd09fbe57
Demetrius Dixon <wilodemetrius@gmail.com>
2026-06-30 09:37:19 -0400

    Updated the quit functions to only send a unregister packet when actually registered

--- utilities/game_quitter.gd
+++ utilities/game_quitter.gd
@@ -2,7 +2,8 @@ extends Node
 
 func quit_monkanics() -> void:
 	
-	Client.unregister_from_ingest_server()
+	if Client.Is_Registered_With_Ingest_Server == true:
+		Client.unregister_from_ingest_server()
 	
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 	
