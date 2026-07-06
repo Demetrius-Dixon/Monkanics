dfd13ff8d9e06d6b37c0e8a0a2efb400897d2d45
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-06 03:30:46 -0400

    Added the server browser to the main menu

--- user_interface/main_menu/main_menu.gd
+++ user_interface/main_menu/main_menu.gd
@@ -1,13 +1,23 @@
 extends Control
 
+@onready var Main_Menu : Node = $"."
+@onready var Main_Screen_Nodes : Node = $MainScreen
+var Server_Browser_Scene : Variant = preload("uid://cltyu1oqpw1v4")
 
-func _on_play_online_pressed() -> void:
+func _ready() -> void:
+	
+	var Server_Browser_To_Load : Variant = Server_Browser_Scene.instantiate()
+	Server_Browser_To_Load.hide()
+	Main_Menu.add_child(Server_Browser_To_Load)
+	Server_Browser_Scene = Server_Browser_To_Load
+
+func _on_play_pressed() -> void:
 	
 	Client.register_to_ingest_server()
 	
+	Main_Screen_Nodes.hide()
 	
-	
-	#TODO Add sub-menu later in development
+	Server_Browser_Scene.show()
 
 func _on_quit_game_pressed() -> void:
 	GameQuitter.quit_monkanics()
