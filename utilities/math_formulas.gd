6cbe0397a4097e461d75d736c78631ace897ccb1
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-16 20:20:21 -0400

    Added rounds per second to math formulas

--- utilities/math_formulas.gd
+++ utilities/math_formulas.gd
@@ -1,5 +1,7 @@
 extends Node
 
 func calculate_actions_per_minute(Actions_Per_Minute:float) -> float:
-	
 	return 60.0 / Actions_Per_Minute
+
+func calculate_actions_per_second(Actions_Per_Second:float) -> float:
+	return 1.0 / Actions_Per_Second
