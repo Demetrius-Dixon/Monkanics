35d0a3da6a8c656b5209c19b049065bca19f467c
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-20 09:40:47 -0400

    Local map loading progress

--- gameplay/gameplay_manager.gd
+++ gameplay/gameplay_manager.gd
@@ -1,9 +1,13 @@
 extends Node
 
 @onready var Main_Scene : Node = $"."
-var Main_Menu_Scene : PackedScene = preload("uid://cuetqgx13tv6s")
-var Server_Browser_Scene : PackedScene = preload("uid://cltyu1oqpw1v4")
+
+var Main_Menu : Node
+var Main_Menu_Preload : PackedScene = preload("uid://cuetqgx13tv6s")
+
 var Gameplay_Scene : PackedScene = preload("uid://f11ymr6ma2f7")
+
+var Current_Map : Variant
 
 func _ready() -> void:
 	
@@ -19,10 +23,27 @@ func _ready() -> void:
 
 func load_main_menu() -> void:
 	
-	var Main_Menu_To_Load := Main_Menu_Scene.instantiate()
+	var Main_Menu_To_Load := Main_Menu_Preload.instantiate()
 	
-	Main_Scene.add_child(Main_Menu_To_Load)
+	Main_Menu = Main_Menu_To_Load
+	
+	Main_Scene.add_child(Main_Menu)
+
+func unload_main_menu() -> void:
+	Main_Menu.queue_free()
+
+func load_current_map() -> void:
+	
+	var Map_To_Load := preload("uid://b1vrb25aej0x1")
+	
+	Current_Map = Map_To_Load
+	
+	Main_Scene.add_child(Current_Map)
 
 func load_game() -> void:
+	
+	unload_main_menu()
+	
+	load_current_map()
 	
 	pass
