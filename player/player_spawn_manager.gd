7defba5cbec24e2ba97d67d0ddcdaa2bbfe84131
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-22 04:36:07 -0400

    Player now spawns

--- player/player_spawn_manager.gd
+++ player/player_spawn_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var Client_Player : Node = null
+var Player_Preload : PackedScene = preload("uid://bv5ucnu7shjsd")
+
 func _ready() -> void:
 	
 	if OS.has_feature("dedicated_server"):
@@ -8,17 +11,19 @@ func _ready() -> void:
 		pass
 
 func spawn_your_player() -> void:
-	pass
 	
-
-func spawn_other_player() -> void:
-	pass
+	var Player_Ref : Node = Player_Preload.instantiate()
 	
+	add_child(Player_Ref)
 
 func despawn_your_player() -> void:
 	pass
 	
 
-func despawn_other_player() -> void:
+func spawn_other_players() -> void:
+	pass
+	
+
+func despawn_other_players() -> void:
 	pass
 	
