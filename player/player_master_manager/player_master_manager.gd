37a8534796cf70ca96e5c1245509c4384c90ec88
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-23 04:13:06 -0400

    Player master manager progress

--- player/player_master_manager/player_master_manager.gd
+++ player/player_master_manager/player_master_manager.gd
@@ -1,7 +1,15 @@
 extends Node
 
-var player_sub_managers : Array[PackedScene] = [
+var player_sub_managers : Array[String] = [
 	
-	
+	"uid://oylpkixktb7v", # Spawn
 	
 ]
+
+func _ready() -> void:
+	load_player_sub_managers()
+
+func load_player_sub_managers() -> void:
+	
+	for manager in player_sub_managers:
+		load(manager)
