017f3555681a072f2547fb4e4368365751e451ef
Demetrius Dixon <wilodemetrius@gmail.com>
2026-07-27 09:40:17 -0400

    Misc commit

--- user_interface/health_bars/player_health/player_health_bar.gd
+++ user_interface/health_bars/player_health/player_health_bar.gd
@@ -1,4 +1,5 @@
 extends ProgressBar
 
-func change_health_value(new_health_value:int) -> void:
+func change_health_value(new_health_value:int) -> int:
 	value = new_health_value
+	return new_health_value
