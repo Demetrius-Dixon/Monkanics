5a582e5b46678daab0413cd18be2dfd1be3fb5bc
Demetrius Dixon <wilodemetrius@gmail.com>
2026-08-18 07:29:48 -0400

    Added ServerBrowser to pruner

--- user_interface/server_browser/server_browser.gd
+++ user_interface/server_browser/server_browser.gd
@@ -1,15 +1,29 @@
 extends Control
 
+@onready var server_list : Node = $"TabContainer/Join Lobby"
+
+var received_active_lobbies : Array[Dictionary] = []
+
 func _ready() -> void:
 	hide()
 
 func _on_create_lobby_pressed() -> void:
 	ClientManager.create_lobby()
 
-func add_lobby_to_server_list(lobby:String) -> void:
+func add_lobby_to_dictionary(lobby:Dictionary) -> void:
 	
-	$"TabContainer/Join Lobby".add_item("lobby")
+	received_active_lobbies.append(lobby)
+	
+	var lobby_name_string : String = lobby[&"host"]
+	
+	print("LOBBY_NAME_STRING ", lobby_name_string)
+	
+	#add_lobbies_to_server_list(lobby[&"host"])
+	
+	#print("SERVER BROWSER: ", received_active_lobbies)
+
+func add_lobbies_to_server_list(lobby_name:String) -> void:
+	server_list.add_item(lobby_name)
 
 func clear_server_browser_cache() -> void:
-	
-	pass
+	received_active_lobbies.clear()
