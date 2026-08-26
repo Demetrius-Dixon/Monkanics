444993fd17f0ef54452971fa53758a9abc01d56d
Demetrius Dixon <wilodemetrius@gmail.com>
2026-08-26 11:36:46 -0400

    Updated math formula names

--- utilities/math/math_manager.gd
+++ utilities/math/math_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-func calculate_actions_per_minute(actions_per_minute:float) -> float:
-	return 60.0 / actions_per_minute
+func calculate_operations_per_minute(operations_per_minute:float) -> float:
+	return 60.0 / operations_per_minute
 
-func calculate_actions_per_second(actions_per_second:float) -> float:
-	return 1.0 / actions_per_second
+func calculate_operations_per_second(operations_per_second:float) -> float:
+	return 1.0 / operations_per_second
